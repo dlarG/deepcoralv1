f3def7732583f7fca8938e4e6deed07c4c677825
AnonJeffz <jeffersonitaok11@gmail.com>
2025-10-27 10:27:33 +0800

    Update API base URL to EC2 backend. Fixed backend requirements and app to be deployed at aws ec2 instance. Deployed psql database to AWS RDS

---
+++ frontend/src/config/api.js
@@ -0,0 +1,5 @@
+// API Configuration
+// Update this URL when deploying to production
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://54.206.75.196";
+
+export default API_BASE_URL;
